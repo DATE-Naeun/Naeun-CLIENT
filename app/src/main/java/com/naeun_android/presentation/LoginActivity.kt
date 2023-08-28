903ea7420f14b07cf5abf49ecc24d4e395cde909
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-08-28 21:30:01 +0900

    [add] #4 - LoginActivity

---
+++ app/src/main/java/com/naeun_android/presentation/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.naeun_android.presentation
+
+import android.os.Bundle
+import com.naeun_android.R
+import com.naeun_android.databinding.ActivityLoginBinding
+import com.naeun_android.util.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+    
+}
