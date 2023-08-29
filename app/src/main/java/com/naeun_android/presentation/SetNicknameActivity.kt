fb6e56e8f417a3900315a9e6ae126bfd0e3c941d
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-08-29 17:20:22 +0900

    [ui] #4 - SetNicknameActivity 뷰 구현

---
+++ app/src/main/java/com/naeun_android/presentation/SetNicknameActivity.kt
@@ -0,0 +1,15 @@
+package com.naeun_android.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.naeun_android.R
+import com.naeun_android.databinding.ActivitySetNicknameBinding
+import com.naeun_android.util.BindingActivity
+
+class SetNicknameActivity : BindingActivity<ActivitySetNicknameBinding>(R.layout.activity_set_nickname) {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
