d1eef8c4ea0d7071765d1eb7be2dd54567c4ec36
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-09-19 01:06:02 +0900

    [ui] #6 - Bottom Navigation 구현

--- app/src/main/java/com/naeun_android/MainActivity.kt
+++ app/src/main/java/com/naeun_android/MainActivity.kt
@@ -1,11 +1,28 @@
 package com.naeun_android
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.naeun_android.databinding.ActivityMainBinding
+import com.naeun_android.presentation.HomeFragment
+import com.naeun_android.util.BindingActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        initView()
+    }
+
+    private fun initView() {
+        val currentFragment = supportFragmentManager.findFragmentById(R.id.fcv_main)
+        if (currentFragment == null) {
+            changeFragment(HomeFragment())
+        }
+    }
+
+    private fun changeFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fcv_main, fragment)
+            .commit()
     }
 }
