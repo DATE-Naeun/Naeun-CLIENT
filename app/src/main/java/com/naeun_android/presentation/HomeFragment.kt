42992763d7031367b2f20ce4a33530075bc5fcb0
yeoncheong <yjin1100@sookmyung.ac.kr>
2023-09-19 17:58:06 +0900

    [ui] #6 - 메인 페이지 구현

---
+++ app/src/main/java/com/naeun_android/presentation/HomeFragment.kt
@@ -0,0 +1,18 @@
+package com.naeun_android.presentation
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.naeun_android.R
+import com.naeun_android.databinding.FragmentHomeBinding
+import com.naeun_android.util.BindingFragment
+
+class HomeFragment : BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
